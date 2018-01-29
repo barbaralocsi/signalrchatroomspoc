458f2dcfe92f7a04925bd699cc355fb7aab95260
Barbara Lócsi <locsi.barbara@gmail.com>
2018-01-29 21:17:14 +0100

    Add groups.

--- SignalRChat/ChatHub.cs
+++ SignalRChat/ChatHub.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 using Microsoft.AspNet.SignalR;
 
@@ -8,10 +9,21 @@ namespace SignalRChat
 {
     public class ChatHub : Hub
     {
-        public void Send(string name, string message)
+        public void Send(string roomName, string name, string message)
         {
             // Call the broadcastMessage method to update clients.
-            Clients.All.broadcastMessage(name, message);
+            //Clients.All.broadcastMessage(name, message);
+            Clients.Group(roomName).addChatMessage(name, message);
+        }
+
+        public Task JoinRoom(string roomName)
+        {
+            return Groups.Add(Context.ConnectionId, roomName);
+        }
+
+        public Task LeaveRoom(string roomName)
+        {
+            return Groups.Remove(Context.ConnectionId, roomName);
         }
     }
 }
